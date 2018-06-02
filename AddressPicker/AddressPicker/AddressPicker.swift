e07aeca4b7686b5683a241fd7f43c99a26f5da9a
IAmFiveHigh <745170149@qq.com>
2018-06-02 17:19:05 +0800

    添加完成按钮 和delegate

--- AddressPicker/AddressPicker/AddressPicker.swift
+++ AddressPicker/AddressPicker/AddressPicker.swift
@@ -11,14 +11,26 @@ import UIKit
 let screenWidth = UIScreen.main.bounds.width
 let screenHeight = UIScreen.main.bounds.height
 
+protocol AddressPickerDelegate: class {
+    func 返回选中省(_ 省: String, 市: String, 县: String, 街道们: [String])
+    
+    func 点击完成()
+}
+
 class AddressPicker: UIView {
 
     private var 省数据源 = [省数据结构]()
     private var 市数据源 = [市数据结构]()
     private var 县数据源 = [县数据结构]()
-    var 街道数据 = [String]()
+    private var 街道数据 = [String]()
     
-    private let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 0))
+    private var 省 = ""
+    private var 市 = ""
+    private var 县 = ""
+    
+    private let pickerView = UIPickerView(frame: CGRect(x: 0, y: 25, width: screenWidth, height: 0))
+    
+    weak var delegate: AddressPickerDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -46,20 +58,46 @@ class AddressPicker: UIView {
             
             市数据源 = 省数据源[0].cities
             县数据源 = 市数据源[0].areas
+            
+            省 = 省数据源[0].state
+            市 = 市数据源[0].city
+            县 = 县数据源[0].county
         }else {
             print("array is not [[string: any]]")
         }
     }
     
     private func setupUI(frame: CGRect) {
+        
+        let line2 = UIView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 1))
+        line2.backgroundColor = UIColor(hexColor: "EBEBEB")
+        self.addSubview(line2)
+        
         var f = pickerView.frame
-        f.size.height = frame.height
+        f.size.height = frame.height - 36
         pickerView.frame = f
         
         pickerView.delegate = self
         pickerView.dataSource = self
         
+        let btn = UIButton(frame: CGRect(x: screenWidth - 40, y: 8, width: 40, height: 20))
+        btn.setTitle("完成", for: .normal)
+        btn.setTitleColor(UIColor(hexColor: "1A1917"), for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        btn.addTarget(self, action: #selector(点击完成), for: .touchUpInside)
+        self.addSubview(btn)
+        
+        let line = UIView(frame: CGRect(x: 0, y: 36, width: screenWidth, height: 1))
+        line.backgroundColor = UIColor(hexColor: "EBEBEB")
+        self.addSubview(line)
+        
         self.addSubview(pickerView)
+    }
+    
+    @objc private func 点击完成() {
+        if let delegate = delegate {
+            delegate.点击完成()
+        }
     }
     
 }
@@ -110,18 +148,28 @@ extension AddressPicker: UIPickerViewDelegate, UIPickerViewDataSource {
             pickerView.reloadComponent(2)
             pickerView.selectRow(0, inComponent: 2, animated: true)
             
+            省 = 省数据源[row].state
+            市 = 市数据源[0].city
+            县 = 县数据源[0].county
+            
             街道数据 = 县数据源[0].streets
         case 1:
             县数据源 = 市数据源[row].areas
             pickerView.reloadComponent(2)
             pickerView.selectRow(0, inComponent: 2, animated: true)
             
+            市 = 市数据源[row].city
+            县 = 县数据源[0].county
+            
             街道数据 = 县数据源[0].streets
         default:
+            
+            县 = 县数据源[row].county
             街道数据 = 县数据源[row].streets
         }
-        
-       
-        
+
+        if let delegate = delegate {
+            delegate.返回选中省(省, 市: 市, 县: 县, 街道们: 街道数据)
+        }
     }
 }
--- AddressPicker/AddressPicker/ViewController.swift
+++ AddressPicker/AddressPicker/ViewController.swift
@@ -10,12 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let b = UILabel(frame: CGRect(x: 0, y: 100, width: screenWidth, height: 50))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         let a = AddressPicker(frame: CGRect(x: 0, y: screenHeight - 200, width: screenWidth, height: 200))
+        a.delegate = self
         view.addSubview(a)
+        
+        view.addSubview(b)
     }
+}
 
+extension ViewController: AddressPickerDelegate {
+    func 点击完成() {
+        
+    }
+    
+    func 返回选中省(_ 省: String, 市: String, 县: String, 街道们: [String]) {
+        print(街道们)
+        b.text = "\(省)\(市)\(县)"
+    }
+    
 }
 

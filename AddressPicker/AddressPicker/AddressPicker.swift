5202279eafb756d831c31ce863b5dca7affcf4d5
IAmFiveHigh <745170149@qq.com>
2018-06-02 15:25:07 +0800

    first commit

---
+++ AddressPicker/AddressPicker/AddressPicker.swift
@@ -0,0 +1,18 @@
+//
+//  AddressPicker.swift
+//  AddressPicker
+//
+//  Created by 每天农资 on 2018/6/2.
+//  Copyright © 2018年 我是五高你敢信. All rights reserved.
+//
+
+import UIKit
+
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
+
+class AddressPicker: UIView {
+
+    
+
+}
